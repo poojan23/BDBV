035d5a847fb2035c1be4f877447389acc637c403
poojan23 <poojan@popaya.in>
2019-07-06 14:21:31 +0530

    added member table to db_schema

--- system/helper/db_schema.php
+++ system/helper/db_schema.php
@@ -411,6 +411,473 @@ function db_schema()
 	);
 
 	$tables[] = array(
+		'name' => 'member',
+		'field' => array(
+			array(
+				'name' => 'member_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'auto_increment' => true
+			),
+			array(
+				'name' => 'member_group_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'language_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'image',
+				'type' => 'varchar(255)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'firstname',
+				'type' => 'varchar(32)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'lastname',
+				'type' => 'varchar(32)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'designation',
+				'type' => 'varchar(64)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'gender',
+				'type' => 'varchar(1)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'email',
+				'type' => 'varchar(96)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'telephone',
+				'type' => 'varchar(32)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'fax',
+				'type' => 'varchar(32)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'birthday',
+				'type' => 'date',
+				'not_null' => true
+			),
+			array(
+				'name' => 'anniversary',
+				'type' => 'date',
+				'not_null' => true
+			),
+			array(
+				'name' => 'lastname',
+				'type' => 'varchar(32)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'password',
+				'type' => 'varchar(255)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'salt',
+				'type' => 'varchar(9)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'newsletter',
+				'type' => 'tinyint(1)',
+				'not_null' => true,
+				'default' => 0
+			),
+			array(
+				'name' => 'address_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'default' => 0
+			),
+			array(
+				'name' => 'ip',
+				'type' => 'varchar(40)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'status',
+				'type' => 'tinyint(1)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'safe',
+				'type' => 'tinyint(1)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'token',
+				'type' => 'text',
+				'not_null' => true
+			),
+			array(
+				'name' => 'code',
+				'type' => 'varchar(40)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_added',
+				'type' => 'datetime',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_modified',
+				'type' => 'datetime',
+				'not_null' => true
+			),
+		),
+		'primary' => array(
+			'member_id'
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
+		'name' => 'member_activity',
+		'field' => array(
+			array(
+				'name' => 'member_activity_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'auto_increment' => true
+			),
+			array(
+				'name' => 'member_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'key',
+				'type' => 'varchar(64)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'data',
+				'type' => 'text',
+				'not_null' => true
+			),
+			array(
+				'name' => 'ip',
+				'type' => 'varchar(40)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_added',
+				'type' => 'datetime',
+				'not_null' => true
+			)
+		),
+		'primary' => array(
+			'member_activity_id'
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
+		'name' => 'member_address',
+		'field' => array(
+			array(
+				'name' => 'member_address_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'auto_increment' => true
+			),
+			array(
+				'name' => 'member_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'firstname',
+				'type' => 'varchar(32)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'lastname',
+				'type' => 'varchar(32)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'company',
+				'type' => 'varchar(60)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'address_1',
+				'type' => 'varchar(128)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'address_2',
+				'type' => 'varchar(128)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'city',
+				'type' => 'varchar(128)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'postcode',
+				'type' => 'varchar(10)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'country_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'default' => 0
+			),
+			array(
+				'name' => 'zone_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'default' => 0
+			)
+		),
+		'primary' => array(
+			'member_address_id'
+		),
+		'index' => array(
+			array(
+				'name' => 'member_id',
+				'key' => array(
+					'member_id'
+				)
+			)
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
+		'name' => 'member_group',
+		'field' => array(
+			array(
+				'name' => 'member_group_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'auto_increment' => true
+			),
+			array(
+				'name' => 'name',
+				'type' => 'varchar(64)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'description',
+				'type' => 'text',
+				'not_null' => true
+			),
+			array(
+				'name' => 'permission',
+				'type' => 'text',
+				'not_null' => true
+			)
+		),
+		'primary' => array(
+			'member_group_id'
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
+		'name' => 'member_history',
+		'field' => array(
+			array(
+				'name' => 'member_history_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'auto_increment' => true
+			),
+			array(
+				'name' => 'member_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'comment',
+				'type' => 'text',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_added',
+				'type' => 'datetime',
+				'not_null' => true
+			)
+		),
+		'primary' => array(
+			'member_history_id'
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
+		'name' => 'member_ip',
+		'field' => array(
+			array(
+				'name' => 'member_ip_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'auto_increment' => true
+			),
+			array(
+				'name' => 'member_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'ip',
+				'type' => 'varchar(40)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'country',
+				'type' => 'varchar(2)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_added',
+				'type' => 'datetime',
+				'not_null' => true
+			)
+		),
+		'primary' => array(
+			'member_ip_id'
+		),
+		'index' => array(
+			array(
+				'name' => 'ip',
+				'key' => array(
+					'ip'
+				)
+			)
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
+		'name' => 'member_login',
+		'field' => array(
+			array(
+				'name' => 'member_login_id',
+				'type' => 'int(11)',
+				'not_null' => true,
+				'auto_increment' => true
+			),
+			array(
+				'name' => 'email',
+				'type' => 'varchar(96)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'ip',
+				'type' => 'varchar(40)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'total',
+				'type' => 'int(4)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_added',
+				'type' => 'datetime',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_modified',
+				'type' => 'datetime',
+				'not_null' => true
+			)
+		),
+		'primary' => array(
+			'member_login_id'
+		),
+		'index' => array(
+			array(
+				'name' => 'email',
+				'key' => array(
+					'email'
+				)
+			),
+			array(
+				'name' => 'ip',
+				'key' => array(
+					'ip'
+				)
+			)
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
+		'name' => 'member_online',
+		'field' => array(
+			array(
+				'name' => 'ip',
+				'type' => 'varchar(40)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'member_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			),
+			array(
+				'name' => 'url',
+				'type' => 'text',
+				'not_null' => true
+			),
+			array(
+				'name' => 'referer',
+				'type' => 'text',
+				'not_null' => true
+			),
+			array(
+				'name' => 'date_added',
+				'type' => 'datetime',
+				'not_null' => true
+			)
+		),
+		'primary' => array(
+			'ip'
+		),
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+		'collate' => 'utf8_general_ci'
+	);
+
+	$tables[] = array(
 		'name' => 'seo_url',
 		'field' => array(
 			array(
