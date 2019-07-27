83fd994d89ec87b7a8812653901768782f2dbc72
poojan23 <poojan@popaya.in>
2019-07-27 17:38:12 +0530

    config file

--- sadmin/config.php
+++ sadmin/config.php
@@ -10,12 +10,12 @@ define('HTTPS_ADMIN', 'http://localhost/bdbv/admin/');
 define('HTTPS_CATALOG', 'http://localhost/bdbv/');
 
 # DIR
-define('DIR_APPLICATION', 'C:/ATDI/LMS/htdocs/bdbv/sadmin/');
-define('DIR_SYSTEM', 'C:/ATDI/LMS/htdocs/bdbv/system/');
-define('DIR_IMAGE', 'C:/ATDI/LMS/htdocs/bdbv/image/');
+define('DIR_APPLICATION', 'C:/xampp/htdocs/bdbv/sadmin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/bdbv/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/bdbv/image/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_CATALOG', 'C:/ATDI/LMS/htdocs/bdbv/template/');
-define('DIR_ADMIN', 'C:/ATDI/LMS/htdocs/bdbv/admin/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/bdbv/template/');
+define('DIR_ADMIN', 'C:/xampp/htdocs/bdbv/admin/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/html/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -30,7 +30,7 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 define('DB_DRIVER', 'pdo');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'bdbv');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'pt_');
